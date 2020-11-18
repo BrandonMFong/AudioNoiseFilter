52f4355d5df91105605e12f2e06e09a0c6401558
BrandonMFong <fong.m.brandon97@gmail.com>
2020-11-18 09:23:43 -0800

    setting up part 1

--- README.m
+++ README.m
@@ -1,28 +1,18 @@
 Setup
 %%% START %%%
 % Brandon Fong 
-% CES 440 Project 2
-% Due Date: Monday, 10/26/2020 
+% CES 440 Project 3
+% Due Date: Monday, 11/24/2020 
 % Name & ID : Brandon Fong (007808236)
 % email: fong.m.brandon97@gmail.com & fongb@sonoma.edu
 % Link to Report: NA
 % Link to Git: NA
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Part 1: Filters
-% M-file name: /src/Part1.m
+% Part 1: the DFT
+% M-file name: 
 % Notes: 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-Filter
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Part 2: AM Channel Modulation
-% M-file name: /src/Part2.m
-% Notes: 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-Modulation
 
 
 %%% END %%%
