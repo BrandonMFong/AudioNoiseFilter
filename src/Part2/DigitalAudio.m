1e718a20dadf4af32176d38a7a0270b8e5a6e762
BrandonMFong <fong.m.brandon97@gmail.com>
2020-11-23 10:13:25 -0800

    playing 440 hz

--- src/Part2/DigitalAudio.m
+++ src/Part2/DigitalAudio.m
@@ -9,3 +9,32 @@ x = cos(w_dig * n);
 
 x = x / max(abs(x));
 sound(x,Fs,16);
+
+X = fftshift(fft(x));
+Xmag = abs(X);
+XmagdB = 20*log10(Xmag);
+
+% plot against radian digital frequency
+w = -pi:2*pi/N:pi-2*pi/N;
+f = w * Fs / (2*pi);
+figure(13);
+plot(f,XmagdB);
+xlim([-20000 20000]);
+title('Centered DFT Magnitude for 440 Hz Pure Tone');
+xlabel('analog frequency, Hz');
+ylabel('dB');
+
+% normalized digital frequency
+figure(14);
+plot(w,XmagdB);
+xlim([-pi pi]);
+title('Centered DFT Magnitude for 440 Hz Pure Tone');
+xlabel('radian digital frequency \omega');
+ylabel('dB');
+
+% wait
+pause(3);
+
+audiowrite('res/A-440.wav',x,Fs);
+[x2,Fs] = audioread('A-440.wav');
+sound(x2,Fs,16);
